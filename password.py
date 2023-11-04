53bc6d57c8a077166669b7ecf0850c2e628f9854
kader <kaderqayed23@gmail.com>
2023-11-04 19:15:45 -0400

    fixed while loop

--- password.py
+++ password.py
@@ -1,14 +1,11 @@
 #password acceptance
 
 print("Hello User \n")
-name = input("Enter your password: ")
+password = input("Enter your password: ")
 
 
-while name != "Master12":
+while password != "Master12":
     print("Try Again \n")
-    name = input("Enter your password: ")
-    if (name == "Master12"):
-        print("Correct \n")
-        break
+    password = input("Enter your password: ")
 
 
